609f153f3314d33fac3509ee664b6ad5b6412a97
AshvittaS <ashvitta07@gmail.com>
2025-11-29 14:17:29 +0530

    Day-18&19 complete

--- Day-18/FirstApp/src/course.jsx
+++ Day-18/FirstApp/src/course.jsx
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types'
+import { useState,useEffect } from 'react';   
 function Course(props){
-    if(props.show == true){
+    const [PurschaseState,Purschased]=useState(false);
+    function buyCourse(){
+        alert(props.name + "Purschased");
+        Purschased(true);
+    }
     return(
         <div className="card"> 
             <img src={props.image}  />
             <h3>{props.name}</h3>
-            <p>{props.price}</p>
+            <p>${props.price}</p>
+            <button onClick={buyCourse}>Buy now</button>
+            <button onClick={()=>props.delete(props.id)}>Delete</button>
+            <p>{PurschaseState ? "Already Purschased" : "Buy it now"}</p>
         </div>
     );
 }
-}
+
 Course.propTypes={
     name:PropTypes.string,
     price:PropTypes.number,
